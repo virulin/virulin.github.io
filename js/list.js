11d083cff90785c0ec823f2cac8bceb4de39d595
virulin <156271061+virulin@users.noreply.github.com>
2024-01-31 12:42:47 -0600

    Create list.js

---
+++ js/list.js
@@ -0,0 +1,16 @@
+  document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector(".btn.btn-primary").addEventListener("click", event => {
+        var itemList = document.getElementById("my-list");
+        var template = document.getElementById("list-template");
+        var total = itemList.childElementCount + 1;
+        var clone = template.content.cloneNode(true);
+        clone.querySelector("[data-id='number']").textContent = `${total}`;
+        clone.querySelector("[data-id='title']").textContent = "Product";
+        clone.querySelector("[data-id='content']").textContent = "It's a new item";
+        itemList.appendChild(clone);
+    });
+    document.querySelector(".btn.btn-light").addEventListener("click", event => {
+        var itemList = document.getElementById("my-list");
+        itemList.replaceChildren();
+    });
+});
