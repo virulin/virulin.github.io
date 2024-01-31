2876fee27d03d85f6334c30741050866cda373ba
virulin <156271061+virulin@users.noreply.github.com>
2024-01-31 13:05:27 -0600

    Update list.js

--- js/list.js
+++ js/list.js
@@ -1,32 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelector(".btn.btn-primary").addEventListener("click", event => {
-        addNewItem("Product", "It's a new item");
+        var itemList = document.getElementById("my-list");
+        var template = document.getElementById("list-template");
+        var total = itemList.childElementCount + 1;
+        var clone = template.content.cloneNode(true);
+        clone.querySelector("[data-id='number']").textContent = `${total}`;
+        clone.querySelector("[data-id='title']").textContent = "Product";
+        clone.querySelector("[data-id='content']").textContent = "It's a new item";
+        itemList.appendChild(clone);
     });
-
     document.querySelector(".btn.btn-light").addEventListener("click", event => {
         var itemList = document.getElementById("my-list");
         itemList.replaceChildren();
     });
 });
-
-function addNewItem(titleText, contentText) {
-    // Crear un nuevo elemento li
-    const newItem = document.createElement("li");
-    newItem.classList.add("item");
-
-    // Añadir el contenido
-    const title = document.createElement("h3");
-    title.textContent = titleText;
-
-    const content = document.createElement("p");
-    content.textContent = contentText;
-
-    // Agregar el contenido al nuevo elemento li
-    newItem.appendChild(title);
-    newItem.appendChild(content);
-
-    // Obtener la lista y agregar el nuevo elemento
-    var itemList = document.getElementById("my-list");
-    itemList.appendChild(newItem);
-}
 
