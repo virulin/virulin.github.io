8d29c954cb4593149f3647cf377af8c11c647b41
virulin <156271061+virulin@users.noreply.github.com>
2024-01-31 12:59:47 -0600

    Update list.js

--- js/list.js
+++ js/list.js
@@ -1,16 +1,25 @@
-  document.addEventListener("DOMContentLoaded", () => {
-    document.querySelector(".btn.btn-primary").addEventListener("click", event => {
+document.addEventListener("DOMContentLoaded", () => {
+    // Función para agregar un nuevo item a la lista
+    function addNewItem(titleText, contentText) {
         var itemList = document.getElementById("my-list");
         var template = document.getElementById("list-template");
         var total = itemList.childElementCount + 1;
         var clone = template.content.cloneNode(true);
         clone.querySelector("[data-id='number']").textContent = `${total}`;
-        clone.querySelector("[data-id='title']").textContent = "Product";
-        clone.querySelector("[data-id='content']").textContent = "It's a new item";
+        clone.querySelector("[data-id='title']").textContent = titleText;
+        clone.querySelector("[data-id='content']").textContent = contentText;
         itemList.appendChild(clone);
+    }
+
+    // Evento click para el botón primario
+    document.querySelector(".btn.btn-primary").addEventListener("click", event => {
+        addNewItem("Product", "It's a new item");
     });
+
+    // Evento click para el botón secundario
     document.querySelector(".btn.btn-light").addEventListener("click", event => {
         var itemList = document.getElementById("my-list");
         itemList.replaceChildren();
     });
 });
+
