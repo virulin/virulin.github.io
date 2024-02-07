7bbaf5af2f29f0846ae7ac49cd05b6e00de557d2
virulin <156271061+virulin@users.noreply.github.com>
2024-02-07 00:42:42 -0600

    Update list.js

--- js/list.js
+++ js/list.js
@@ -1,17 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const personajes  =["monito1", "monito2", "monito3", "monito4", "monito5",];
+    let currentpersonajeindex = 0;
+    
     document.querySelector(".btn.btn-primary").addEventListener("click", event => {
+        const personajeToAdd = personajes [currentpersonajeindex];
+        if (personajeToAdd){
         var itemList = document.getElementById("my-list");
         var template = document.getElementById("list-template");
         var total = itemList.childElementCount + 1;
         var clone = template.content.cloneNode(true);
         clone.querySelector("[data-id='number']").textContent = `${total}`;
-        clone.querySelector("[data-id='title']").textContent = "Product";
-        clone.querySelector("[data-id='content']").textContent = "It's a new item";
+        clone.querySelector("[data-id='title']").textContent = personajeToAdd;
+        clone.querySelector("[data-id='content']").textContent = `Description of (personajeToAdd)`;
         itemList.appendChild(clone);
+
+            currentCharacterIndex++;
     });
     document.querySelector(".btn.btn-light").addEventListener("click", event => {
         var itemList = document.getElementById("my-list");
-        itemList.replaceChildren();
+        itemList.personajeindex = 0;
     });
 });
 
