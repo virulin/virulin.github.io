7c35d6f6c3d34e03818afb7db8dc55765bbdcd39
virulin <156271061+virulin@users.noreply.github.com>
2024-02-07 00:50:54 -0600

    Update list.js

--- js/list.js
+++ js/list.js
@@ -1,24 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const personajes  =["monito1", "monito2", "monito3", "monito4", "monito5",];
+    const personajes = ["monito1", "monito2", "monito3", "monito4", "monito5"];
     let currentpersonajeindex = 0;
-    
-    document.querySelector(".btn.btn-primary").addEventListener("click", event => {
-        const personajeToAdd = personajes [currentpersonajeindex];
-        if (personajeToAdd){
-        var itemList = document.getElementById("my-list");
-        var template = document.getElementById("list-template");
-        var total = itemList.childElementCount + 1;
-        var clone = template.content.cloneNode(true);
-        clone.querySelector("[data-id='number']").textContent = `${total}`;
-        clone.querySelector("[data-id='title']").textContent = personajeToAdd;
-        clone.querySelector("[data-id='content']").textContent = `Description of (personajeToAdd)`;
-        itemList.appendChild(clone);
 
-            currentCharacterIndex++;
+    document.querySelector(".btn.btn-primary").addEventListener("click", event => {
+        const personajeToAdd = personajes[currentpersonajeindex];
+        if (personajeToAdd) {
+            var itemList = document.getElementById("my-list");
+            var template = document.getElementById("list-template");
+            var total = itemList.childElementCount + 1;
+            var clone = template.content.cloneNode(true);
+            clone.querySelector("[data-id='number']").textContent = `${total}`;
+            clone.querySelector("[data-id='title']").textContent = personajeToAdd;
+            clone.querySelector("[data-id='content']").textContent = `Description of ${personajeToAdd}`;
+            itemList.appendChild(clone);
+
+            currentpersonajeindex++;
+        }
     });
+
     document.querySelector(".btn.btn-light").addEventListener("click", event => {
         var itemList = document.getElementById("my-list");
-        itemList.personajeindex = 0;
+        currentpersonajeindex = 0;
     });
 });
+
 
